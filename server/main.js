7487846d19ae9e63abc761da96e324cedd084728
Jovan El Hoayek <jhoayek@gmail.com>
2017-12-01 11:03:12 -0500

    Use HTTP2 and redirect HTTP to HTTPS

--- server/main.js
+++ server/main.js
@@ -2,19 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
+const compression = require('compression');
+const spdy = require('spdy');
+const fs = require('fs');
+const http = require('http');
 
 const config = require('./config');
 const api = require('./api');
 
-const app = express();
-const port = process.env.PORT || 3000;
+// Redirect HTTP to HTTPS
+http.createServer((req, res) => {
+  res.writeHead(301, { Location: 'https://' + req.headers['host'] + req.url });
+  res.end();
+}).listen(8000, (error) => {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  console.log('HTTP Listening to port 8000');
+});
 
+const app = express();
+const port = +process.env.PORT || 3000;
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'server.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'server.crt'))
+};
+
+app.use(compression());
 app.use(bodyParser.text());
 app.use(session({
   secret: config.session_secret,
   resave: false,
   saveUninitialized: false
 }));
+app.u
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
@@ -24,6 +46,10 @@ app.get('*/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+spdy.createServer(options, app).listen(port, (error) => {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  console.log(`HTTPS listening on port ${port}...`);
 });
